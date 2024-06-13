84dfc0385eb9d2dbc0c78e189aa22656fba27850
cperez1703 <98626649+cperez1703@users.noreply.github.com>
2024-06-13 17:29:53 -0400

    added server sidebar

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -30,7 +30,7 @@ export const NavigationSidebar = async () => {
 
     return(
         <div
-            className="space-y-4 flex flex-col items-center h-full text-primary w-full bg-[#f3f4f6] dark:bg-[#1E1F22] py-3"
+            className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3"
         >
             <NavigationAction/>
             <Separator
