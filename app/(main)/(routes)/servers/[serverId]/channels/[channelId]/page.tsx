47345bec2d3882d79fc6e3518627d6158f9e0ae1
cperez1703 <98626649+cperez1703@users.noreply.github.com>
2024-06-22 22:38:35 -0400

    chat header

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,51 @@
-const ChannelIdPage = () => {
+import { ChatHeader } from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+interface ChannelIdPageProps{
+    params:{
+        serverId: string;
+        channelId: string;
+    }
+}
+
+const ChannelIdPage = async ({
+    params
+}: ChannelIdPageProps) => {
+    const profile = await currentProfile();
+
+    if(!profile){
+        return auth().redirectToSignIn();
+    }
+
+    const channel = await db.channel.findUnique({
+        where: {
+            id: params.channelId,
+        },
+
+    })
+
+    const member = await db.member.findFirst({
+        where: {
+            serverId: params.serverId,
+            profileId: profile.id,
+        }
+    })
+
+    if(!channel || !member){
+        redirect("/");
+    }
+
     return ( 
-        <div>
-            Channel ID page
+        <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+            <ChatHeader
+                name={channel.name}
+                serverId={channel.serverId}
+                type="channel"
+            />
         </div>
      );
 }
