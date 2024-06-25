aaf78794b47f2d88414f75944ccc412f1cb29b75
cperez1703 <98626649+cperez1703@users.noreply.github.com>
2024-06-24 20:14:28 -0400

    socket io

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ import {
 } from '@clerk/nextjs'
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
+import { SocketProvider } from "@/components/providers/socket-provider";
 import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ["latin"] });
@@ -37,8 +38,10 @@ export default function RootLayout({
             enableSystem={false}
             storageKey="discord-theme"
           >
-            <ModalProvider/>
-            {children}
+            <SocketProvider>
+              <ModalProvider/>  
+              {children}
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </html>
