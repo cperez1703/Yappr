d390227a649d4f0113dee45096eb80cec1635570
cperez1703 <98626649+cperez1703@users.noreply.github.com>
2024-06-27 18:31:41 -0400

    chat socket hook

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -7,6 +7,8 @@ import { Loader2, ServerCrash } from "lucide-react";
 import { Fragment } from "react";
 import { ChatItem } from "./chat-item";
 import { format } from "date-fns";
+import { useChatSocket } from "@/hooks/use-chat-socket";
+
 
 const DATE_FORMAT = "d MMM yyy, HH:mm";
 
@@ -40,6 +42,8 @@ export const ChatMessages = ({
     type,
 }: ChatMessagesProps) => {
     const queryKey = `chat:${chatId}`
+    const addKey = `chat:${chatId}:messages`
+    const updateKey = `chat:${chatId}:messages:update`
 
     const {
         data,
@@ -53,6 +57,8 @@ export const ChatMessages = ({
         paramKey,
         paramValue,
     });
+
+    useChatSocket({queryKey, addKey, updateKey})
 
     if(status === "loading"){
         return(
