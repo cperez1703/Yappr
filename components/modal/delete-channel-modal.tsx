9ded9b0e798f4d4eee72ca0ef6793aaad6bfa877
cperez1703 <98626649+cperez1703@users.noreply.github.com>
2024-06-29 19:06:23 -0400

    delete channel refresh

--- components/modal/delete-channel-modal.tsx
+++ components/modal/delete-channel-modal.tsx
@@ -44,6 +44,7 @@ export const DeleteChannelModal = () => {
             console.error(error);
         } finally {
             setIsLoading(false);
+            router.refresh();
         }
     }
     
