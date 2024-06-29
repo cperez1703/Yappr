eb444ee905528ca277e67366a88c65f1a1aebc98
cperez1703 <98626649+cperez1703@users.noreply.github.com>
2024-06-29 02:29:45 -0400

    loading change

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -72,16 +72,16 @@ export const ChatMessages = ({
         count: data?.pages?.[0]?.items.length ?? 0,
     });
 
-    if(status === "loading"){
-        return(
-            <div className="flex flex-col flex-1 justify-center items-center">
-                <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4"/>
-                <p className="text-xs text-zinc-500 dark:text-zinc-400">
-                    Loading Messages...
-                </p>
-            </div>
-        )
-    }
+    // if(status === "loading"){
+    //     return(
+    //         <div className="flex flex-col flex-1 justify-center items-center">
+    //             <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4"/>
+    //             <p className="text-xs text-zinc-500 dark:text-zinc-400">
+    //                 Loading Messages...
+    //             </p>
+    //         </div>
+    //     )
+    // }
 
     if(status === "error"){
         return(
